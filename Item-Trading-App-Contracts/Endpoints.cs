d808bc739773e41ec6e554ea626e9504ab15e4a8
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-08-23 21:34:12 +0300

    Add endpoints

---
+++ Item-Trading-App-Contracts/Endpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace Item_Trading_App_Contracts
+{
+    public static class Endpoints
+    {
+        public static class Identity
+        {
+            private static readonly string Base = "identity";
+
+            public static readonly string Register = Base + "/register";
+
+            public static readonly string Login = Base + "/login";
+
+            public static readonly string Refresh = Base + "/refresh";
+        }
+    }
+}
