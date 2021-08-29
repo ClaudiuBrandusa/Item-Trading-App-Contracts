f0ddc25e1750d0e46963d0fc07eb96239af9ba1d
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-08-29 15:27:42 +0300

    Add endpoints for Item, Trade & Wallet controllers

--- Item-Trading-App-Contracts/Endpoints.cs
+++ Item-Trading-App-Contracts/Endpoints.cs
@@ -4,13 +4,47 @@
     {
         public static class Identity
         {
-            private const string Base = "identity";
+            private const string Base = "identity/";
 
-            public const string Register = Base + "/register";
+            public const string Register = Base + "register";
 
-            public const string Login = Base + "/login";
+            public const string Login = Base + "login";
 
-            public const string Refresh = Base + "/refresh";
+            public const string Refresh = Base + "refresh";
+        }
+
+        public static class Item
+        {
+            private const string Base = "item/";
+
+            public const string Create = Base + "create";
+
+            public const string Get = Base + "get";
+
+            public const string List = Base + "list";
+
+            public const string Add = Base + "add";
+
+            public const string Update = Base + "update";
+
+            public const string Delete = Base + "delete";
+        }
+
+        public static class Trade
+        {
+            private const string Base = "trade/";
+
+            public const string Create = Base + "create";
+
+            public const string Accept = Base + "accept";
+
+            public const string Reject = Base + "reject";
+
+            public const string Get = Base + "get";
+
+            public const string GetAll = Base + "getall";
+
+            public const string Cancel = Base + "cancel";
         }
     }
 }
