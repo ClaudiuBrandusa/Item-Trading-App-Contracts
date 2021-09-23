57afc3719d738fba46f727a484369e6319e8ec80
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-09-23 12:07:16 +0300

    Remove request contracts used on GET actions

--- Item-Trading-App-Contracts/Requests/Item/GetItemRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Item_Trading_App_Contracts.Requests.Item
-{
-    // used when we need details about an item
-    public class GetItemRequest
-    {
-        public string ItemId { get; set; }
-    }
-}
--- Item-Trading-App-Contracts/Requests/Trade/GetTradeOfferRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Item_Trading_App_Contracts.Requests.Trade
-{
-    // used when we want details about a trade offer
-    public class GetTradeOfferRequest
-    {
-        public string TradeId { get; set; }
-    }
-}
