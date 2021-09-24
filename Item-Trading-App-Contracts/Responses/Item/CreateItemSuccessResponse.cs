cc7e0e527c234d55aa11dbba36e2741a6691a899
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-09-24 10:46:48 +0300

    Add create item success response contract

---
+++ Item-Trading-App-Contracts/Responses/Item/CreateItemSuccessResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Item_Trading_App_Contracts.Responses.Item
+{
+    public class CreateItemSuccessResponse
+    {
+        public string ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ItemDescription { get; set; }
+    }
+}
