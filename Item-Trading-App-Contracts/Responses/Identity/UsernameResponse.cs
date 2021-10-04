ff5a948f301a19fab1f8fc87da4968b2f35ae029
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-10-04 12:42:46 +0300

    Add username response contract

---
+++ Item-Trading-App-Contracts/Responses/Identity/UsernameResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Item_Trading_App_Contracts.Responses.Identity
+{
+    public class UsernameResponse
+    {
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
