9b192f1d55c42572bc60b9eeb0314c659e2ae084
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-10-22 18:08:11 +0300

    Fix user register request contract

--- Item-Trading-App-Contracts/Requests/Identity/UserRegisterRequest.cs
+++ Item-Trading-App-Contracts/Requests/Identity/UserRegisterRequest.cs
@@ -4,6 +4,10 @@
     {
         public string Username { get; set; }
 
+        public string Email { get; set; }
+
         public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
