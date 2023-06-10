1df528ca5526ecd40ea8d044182fa2ef4f0073fd
Claudiu Brandusa <claudiu.office99@gmail.com>
2023-06-10 18:57:37 +0300

    Add token expiration date to the Authentication response

--- Item-Trading-App-Contracts/Responses/Identity/AuthenticationSuccessResponse.cs
+++ Item-Trading-App-Contracts/Responses/Identity/AuthenticationSuccessResponse.cs
@@ -1,9 +1,13 @@
-﻿namespace Item_Trading_App_Contracts.Responses.Identity
+﻿using System;
+
+namespace Item_Trading_App_Contracts.Responses.Identity
 {
     public class AuthenticationSuccessResponse
     {
         public string Token { get; set; }
 
         public string RefreshToken { get; set; }
+
+        public DateTime ExpirationDateTime { get; set; }
     }
 }
